c28f799f630a1f70067c0c7f57de2a74e7661552
Isioma Mabatah <Mabatah93@gmail.com>
2021-04-02 19:23:21 -0500

    Conference

--- combine.R
+++ combine.R
@@ -11,67 +11,70 @@ library(factoextra)
 # Data ----
 nfl <- read_csv("Data/NFLCombine.csv")
 
-# More Cleaning
-nfl$school <- nfl$school %>% str_replace(pattern = "St",replacement = "ate")
-nfl$school %>% str_replace(pattern = "atete",replacement = "State")
+# Clean: "St. & St" to State\
+nfl$school <- nfl$school %>% str_replace(pattern = "St.", replacement = "State")
+nfl$school <- nfl$school %>% str_replace(pattern = "St", replacement = "State")
+nfl$school <- nfl$school %>% str_replace(pattern = "Stateate",replacement = "State")
+nfl$school <- nfl$school %>% str_replace(pattern = "Statete",replacement = "State")
+nfl$school <- nfl$school %>% str_replace(pattern = "Statenford",replacement = "Stanford")
 
-# - clean: Draft Variables
-# -- Spilt to Columns: Team | Round | Pick | Draft_Year
+
+# clean: Split Draft information "drafted"
+# - Spilt to Columns: Team | Round | Pick | Draft_Year
 draft <- nfl$drafted %>% str_split(pattern = "/", simplify = TRUE) %>% as_tibble() %>% 
   select(team = V1, round = V2, pick = V3, draft_year = V4)
-# -- draft: team
+# - team: remove white space at end
 draft$team <- draft$team %>% str_trim(side = "right")
-# -- draft: round
-draft$round <- draft$round %>% str_remove_all(pattern = " ")
-# -- draft: pick
-draft$pick <- draft$pick %>% str_remove(pattern = " ")
-draft$pick <- draft$pick %>% str_remove(pattern = "st pick")
-draft$pick <- draft$pick %>% str_remove(pattern = "nd pick")
-draft$pick <- draft$pick %>% str_remove(pattern = "rd pick")
-draft$pick <- draft$pick %>% str_remove(pattern = "th pick")
-draft$pick <- draft$pick %>% str_remove(pattern = " ")
-# -- draft: year
+# - round: remove white space
+draft$round <- draft$round %>% str_trim(side = "both")
+# - pick: remove "pick"
+draft$pick <- draft$pick %>% str_remove(pattern = " pick ")
+# -- draft_year: remove white space
 draft$draft_year <- draft$draft_year %>% str_trim(side = "left")
-# -- Draft: NAs
+# - Draft: change "" to NA
 draft <- draft %>% 
   mutate(round = ifelse(round == "", NA, round),
          pick = ifelse(pick == "", NA, pick),
          draft_year = ifelse(draft_year == "", NA, draft_year))
 
-# Add to NFL dataset & change DataTypes
+# Add Split Draft Info back to Dataframe
 nfl <- nfl %>% 
-  bind_cols(draft) %>% 
-  mutate(position = position %>% factor,
-         year = year %>% factor,
-         drafted = ifelse(is.na(drafted), "No","Yes") %>% factor,
-         round = round %>% factor,
-         draft_year = draft_year %>% factor)
+  bind_cols(draft)
 
-# Offense-Defense
+# - Change the old drafted variable to a tag
+nfl <- nfl %>% 
+  mutate(drafted = ifelse(is.na(drafted), "No","Yes") %>% factor)
+
+# - DataType Conversions
+nfl <- nfl %>% 
+  # Do not need "draft_year" already have "year"
+  select(-draft_year) %>% 
+  mutate(position = position %>% factor,
+         school = school %>% factor,
+         year = year %>% factor,
+         team = team %>% factor,
+         round = round %>% factor,
+         pick = pick %>% factor)
+
+# Feature Engineering
+# - Offense-Defense
 nfl <- nfl %>% 
   mutate(side = ifelse(position %in% c("WR","OT","RB","OG","TE","C","FB","QB","LS","OL"), "Offense", "Defense"))
-# - offense: 1426
-nfl %>% 
-  filter(side == "Offense") %>% 
-  count()
-# - 1459
-nfl %>% 
-  filter(side == "Defense") %>% 
-  count()
-
-# Conference
+# - Conference
 nfl <- nfl %>% 
   mutate(conference = case_when(
-    school %in% c("Northwestern", "Iowa", "Wisconsin", "Minnesota","Nebraska","Purdue","Illinois","Ohio State","Indiana","Penn St.","Maryland","Rutgers","Michigan","Michigan St.") ~ "Big 10",
-    school %in% c("Iowa State", "Oklahoma","Oklahoma St.","Texas","TCU","West Virginia", "Kansas State", "Texas Tech", "Baylor","Kansas") ~ "Big 12",
+    school %in% c("Northwestern", "Iowa", "Wisconsin", "Minnesota","Nebraska","Purdue","Illinois","Ohio State","Indiana","Penn State","Maryland","Rutgers","Michigan","Michigan State") ~ "Big 10",
+    school %in% c("Iowa State", "Oklahoma","Oklahoma State","Texas","TCU","West Virginia", "Kansas State", "Texas Tech", "Baylor","Kansas") ~ "Big 12",
     school %in% c("Cincinnati", "Tulsa", "Memphis", "UCF", "SMU", "Houston", "Navy", "Tulane", "East Carolina", "Temple", "South Florida") ~ "American Athletic Conference",
-    school %in% c("Notre Dame","Clemson","Miami","North Carolina", "North Carolina St.", "Boston College","Pittsburgh","Virginia Tech","Virginia","Wake Forrest","Georgia Tech","Louisville","Florida State","Duke","Syracuse") ~ "Atlantic Coast",
+    school %in% c("Notre Dame","Clemson","Miami (FL)","North Carolina", "North Carolina State", "Boston College","Pittsburgh","Virginia Tech","Virginia","Wake Forrest","Georgia Tech","Louisville","Florida State","Duke","Syracuse") ~ "Atlantic Coast",
     school %in% c("Marshall", "Florida Atlantic","Western Kentucky","Charlotte","Old Dominion","Middle Tennesse","Florida International","Texas-San Antonio","North Texas","Rice","Southern Miss","Texas-El Paso") ~ "Conference USA",
-    school %in% c("USC","Colorado","Utah","Arizona State","UCLA","Arizona","Washington","Oregon","Stanford","Oregon State","Washington St.","California") ~ "Pac-12",
+    school %in% c("USC","Colorado","Utah","Arizona State","UCLA","Arizona","Washington","Oregon","Stanford","Oregon State","Washington State","California") ~ "Pac-12",
     school %in% c("Alabama","Texas A&M","Auburn","LSU","Mississippi","Arkansas","MS State","Florida","Georgia","Missouri","Kentucky","Tennessee", "South Carolina","Vanderbilt") ~ "Southeastern",
     school %in% c("Buffalo", "Kent State","Ohio","Miami OH","Akron","Bowling Green","Ball State","Western Michigan", "Toledo","Central Michigan","Eastern Michigan","Northern Illinois") ~ "Mid-American",
-    school %in% c("San Jose St.", "Boise St.","Nevada","San Diego St.","Hawaii","Fresno St.", "Air Force","Wyoming","New Mexico","Colorado State","Utah State","UNLV") ~ "Mountain West",
-    school %in% c("North Dakota St.") ~ "Missouri Valley"
+    school %in% c("San Jose State", "Boise State","Nevada","San Diego State","Hawaii","Fresno State", "Air Force","Wyoming","New Mexico","Colorado State","Utah State","UNLV") ~ "Mountain West",
+    school %in% c("Fordham") ~ "Atlantic 10",
+    school %in% c("Maine") ~ " America East Conference",
+    school %in% c("North Dakota State") ~ "Missouri Valley"
   ))
 
 # Exploratory Data Analysis: Offense ----
